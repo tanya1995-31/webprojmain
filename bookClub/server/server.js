1b87766aa28778db3d518644d85fa3d2823e061b
tanya1995-31 <74137847+tanya1995-31@users.noreply.github.com>
2024-03-30 23:48:40 +0300

    Update server.js

--- bookClub/server/server.js
+++ bookClub/server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS options
 const corsOptions = {
-  origin: 'https://webprojmain.vercel.app', // The origin of the frontend application
+  origin: '', // The origin of the frontend application
   credentials: true, // Allows cookies to be sent
   methods:["POST","GET"],
 };
