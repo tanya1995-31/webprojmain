3982c4370bfd6d5b30d2f6405cd48c8c4f016dae
Omri Shalev <shalevomri10@gmail.com>
2024-04-19 15:48:21 +0300

    contactus_spaces fix

--- bookClub/src/components/ContactUs.js
+++ bookClub/src/components/ContactUs.js
@@ -9,34 +9,33 @@ const ContactUs = ({ isDarkMode }) => {
     { id: 4, name: "Almog Kadosh", email: "almog@gmail.com" }
   ];
 
-  // Styles
-  const memberContainerStyle = `p-4 rounded-md border ${isDarkMode ? 'bg-gray-800 border-gray-600' : 'bg-white border-gray-300'}`;
-  const textStyle = `mt-4 text-xl font-mono mx-auto leading-relaxed max-w-2xl ${isDarkMode ? 'text-white' : 'text-gray-700 '}`; 
-  const memberTextStyle = `font-semibold ${isDarkMode ? 'text-white' : 'text-gray-900'}`;
-  const memberEmailStyle = `text-blue-500 ${isDarkMode ? 'dark:text-blue-400' : ''}`;
-  
-  return (
-    <div>
-      <Header header='Contact Us' isDarkMode={isDarkMode}/>
-      <div className="min-h-screen flex flex-col text-center justify-center items-center py-12">
-        <p className={textStyle}>
-          If you've encountered any problems while using our site or if you have any questions, feel free to reach out to any of our team members listed below. We're here to help and ensure you have the best experience possible.
-        </p>
-        <br />
-        {/* Adjusted the section to start higher on the page */}
-        <section className="w-full max-w-2xl px-6 pb-12">
-          <div className="space-y-4">
-            {members.map((member) => (
-              <div key={member.id} className={`${memberContainerStyle} flex justify-between items-center`}>
-                <p className={memberTextStyle}>{member.name}</p>
-                <a href={`mailto:${member.email}`} className={memberEmailStyle}>{member.email}</a>
-              </div>
-            ))}
-          </div>
-        </section>  
-      </div>
-    </div>
-  );
+ // Styles
+ const containerStyle = `flex flex-col text-center items-center pt-4 ${isDarkMode ? 'bg-gray-800' : 'bg-gray-100'}`; // Reduced top padding
+ const textStyle = `text-xl font-mono mx-auto leading-relaxed max-w-2xl ${isDarkMode ? 'text-white' : 'text-gray-700 '}`;
+ const memberContainerStyle = `p-4 rounded-md border ${isDarkMode ? 'bg-gray-800 border-gray-600' : 'bg-white border-gray-300'}`;
+ const memberTextStyle = `font-semibold ${isDarkMode ? 'text-white' : 'text-gray-900'}`;
+ const memberEmailStyle = `text-blue-500 ${isDarkMode ? 'dark:text-blue-400' : ''}`;
+
+ return (
+   <div>
+     <Header header='Contact Us' isDarkMode={isDarkMode}/>
+     <div className={containerStyle}> 
+       <p className={textStyle}>
+         If you've encountered any problems while using our site or if you have any questions, feel free to reach out to any of our team members listed below. We're here to help and ensure you have the best experience possible.
+       </p>
+       <section className="w-full max-w-2xl px-6 pb-12">
+         <div className="space-y-4 mt-4"> {/* You can adjust the margin here */}
+           {members.map((member) => (
+             <div key={member.id} className={`${memberContainerStyle} flex justify-between items-center`}>
+               <p className={memberTextStyle}>{member.name}</p>
+               <a href={`mailto:${member.email}`} className={memberEmailStyle}>{member.email}</a>
+             </div>
+           ))}
+         </div>
+       </section>  
+     </div>
+   </div>
+ );
 };
 
 export default ContactUs;
