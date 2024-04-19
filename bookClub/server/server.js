bbe7d413bfc65561aaada180bed37261f0faf06d
tanya1995-31 <tanyagendelman@gmail.com>
2024-04-19 16:49:47 +0300

    final

--- bookClub/server/server.js
+++ bookClub/server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS options
 const corsOptions = {
-  origin: 'http://localhost:3000', // The origin of the frontend application
+  origin: 'https://webprojmainserver.vercel.app/', // The origin of the frontend application
   credentials: true, // Allows cookies to be sent
 };
 
